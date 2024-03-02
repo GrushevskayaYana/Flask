84c86a066990dfb4900e0cd3bf6c4ded2d54d6a6
GrushevskayaYana <grushevskayayana712@gmail.com>
2024-03-02 20:57:57 +0200

    fourth lesson complete

--- app.py
+++ app.py
@@ -19,6 +19,18 @@ def index():
     )
 
 
+# Invalid URL
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template("500.html"), 500
+
+
+# Internal Server Error
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html", error=e), 404
+
+
 if __name__ == '__main__':
     app.run(
         debug=True,
