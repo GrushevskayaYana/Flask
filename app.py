21c5740a31f04c8f8fe65f3b1fd20a4d3caf6a52
GrushevskayaYana <grushevskayayana712@gmail.com>
2024-03-11 18:16:52 +0200

    lesson 5 complete

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = "my super secret key that no one is supposed to know"
+
+
+# Create a Form Class
+class NamerForm(FlaskForm):
+    name = StringField("What's Your Name", validators=[DataRequired()])
+    submit = SubmitField("Submit")
 
 
 @app.route("/user/<user_name>")
@@ -21,14 +32,29 @@ def index():
 
 # Invalid URL
 @app.errorhandler(500)
-def page_not_found(e):
-    return render_template("500.html"), 500
+def page_not_found(error):
+    return render_template("500.html", error=error), 500
 
 
 # Internal Server Error
 @app.errorhandler(404)
-def page_not_found(e):
-    return render_template("404.html", error=e), 404
+def page_not_found(error):
+    return render_template("404.html", error=error), 404
+
+
+# Create Name Page
+@app.route('/name', methods=['GET', 'POST'])
+def _name():
+    name = None
+    form = NamerForm()
+    # Validate Form
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+
+    return render_template("name.html",
+                           name=name,
+                           form=form)
 
 
 if __name__ == '__main__':
